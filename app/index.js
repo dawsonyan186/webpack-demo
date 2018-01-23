ad59312076534b58e088fb64f0add20626411611
dawsonyan186 <326814950@qq.com>
2018-01-23 23:13:16 +0800

    1.应用自己定义的模块 2.压缩输出文件

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 import _ from 'lodash'
 import j from 'jquery'
+import dawson from './foo'
 
 function component () {
     var element = j('<div></div>');
     element.html(_.join(['Hello','webpack'], ' '))
-  
+    console.log(dawson());
     return element.get(0);
   }
   
