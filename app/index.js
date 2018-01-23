9447574f4dc1cf652f5af60406f000c371fca946
dawsonyan186 <326814950@qq.com>
2018-01-23 22:56:35 +0800

    加入jquery

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
+import _ from 'lodash'
+import j from 'jquery'
+
 function component () {
-    var element = document.createElement('div');
+    var element = j('<div></div>');
+    element.html(_.join(['Hello','webpack'], ' '))
   
-    /* lodash is required for the next line to work */
-    element.innerHTML = _.join(['Hello','webpack'], ' ');
-  
-    return element;
+    return element.get(0);
   }
   
   document.body.appendChild(component());
